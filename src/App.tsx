93304f6812f2c15dc7f389ef8fb3ea61106f80e4
drozdowska-maria <drozdowska.maria1@gmail.comm>
2023-07-14 00:18:30 +0200

    cta animation in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CTAAnimation from "./components/animations/CTAAnimation/CTAAnimation";
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
+      <CTAAnimation />
     </div>
   );
 }
